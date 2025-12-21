823813894c771a597611748e9ca8af956c6f44e5
BekirTugraKarabulut <bekirtugrakarabulut@gmail.com>
2025-12-21 14:15:08 +0300

    First Commit
    First Commit

---
+++ galeriProject/src/test/java/com/galeri/galeriProject/GaleriProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galeri.galeriProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaleriProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
